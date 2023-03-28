4e0b67d3d4ae524d11213e8f5471f10d90a532be
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-28 19:31:02 +0200

    modify function

--- palindroma/README.md
+++ palindroma/README.md
@@ -6,11 +6,9 @@ Creare una funzione per capire se la parola inserita è palindroma.
 
 ## STEPS
 
-- Creare un prompt per chiedere all'utente di inserire una parola;
-- Suddivido la parola;
-- Inverto l'ordine delle lettere;
-- Creo una nuova stringa;
-- 
+- Creo un prompt per chiedere all'utente di inserire una parola;
 
+- Creo una condizione per confrontare la parola con la parola al contrario;
 
+- Creo una funzione per dividere la parola e poi creare un nuovo array
 
--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,11 +2,19 @@
 // const wordUser = prompt('inserisci una parola palindroma');
 
 //chiedo all'utente di inserire una parola palindroma
-const word = 'annab';
-//Suddivido la parola
-let wordArr = word.split('')
-console.log(wordArr);
-//Utilizzo il metodo Reverse per invertire l'ordine degli elementi
-let wordSplit = wordArr.reverse();
-//riunisco le lettere per ottenere una nuova stringa
-let wordArr2 = wordSplit.join('');
+const word = prompt('inserisci una parola');
+const wordReverse = wordArr(word);
+
+//Creo una condizione per cui verifichiamo che la parola sia uguale alla parola al contrario
+if (word === wordReverse) {
+  console.log('la parola è palindroma');
+} else {
+  console.log('la parola non è palindroma');
+}
+
+//Faccio una funzione
+function wordArr(str) {
+  let strReverse = str.split('').reverse().join('');
+
+  return strReverse;
+}
